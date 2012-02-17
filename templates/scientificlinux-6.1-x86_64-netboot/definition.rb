49fb103733236e91f5f32a8dc7e4338fcfa7138c
Karel Vervaeke <karel@outerthought.org>
2012-02-17 16:54:10 +0100

    Increased ssh_login_timeout. 100 seconds is too short.

--- templates/scientificlinux-6.1-x86_64-netboot/definition.rb
+++ templates/scientificlinux-6.1-x86_64-netboot/definition.rb
@@ -16,7 +16,7 @@ Veewee::Session.declare({
   :kickstart_port => "7122",
   :kickstart_timeout => 10000,
   :kickstart_file => "ks.cfg",
-  :ssh_login_timeout => "100",
+  :ssh_login_timeout => "10000",
   :ssh_user => "vagrant",
   :ssh_password => "vagrant",
   :ssh_key => "",
